6f84ea18b1663a858619811556312d2011460b92
Yerkebulan <erkebulanaza@gmail.com>
2026-03-11 01:11:41 +0500

    Добавлены служебные файлы проекта

---
+++ frontend/payment-form/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <nav>
+    <router-link to="/" class="link">Create payment</router-link> |
+    <router-link to="/history" class="link">History</router-link> |
+    <router-link to="/stats" class="link">Stats</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style scoped>
+.link{
+  text-decoration: none;
+  font-size: 24px;
+  color: darkgreen;
+}
+</style>
